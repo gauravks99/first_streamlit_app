5733c3511e3a64c412157a685b49220fb7697032
Gaurav Kumar Singh <gaurav.mnnita@gmail.com>
2023-07-24 17:38:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import requests
 import pandas
 import snowflake.connector
 from urllib.error import URLError
@@ -22,16 +23,26 @@ streamlit.dataframe(fruits_to_show)
 
 #new request to display fruityvice api response
 streamlit.header('Fruityvice fruit advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+try :
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice :
+  streamlit.error("Please select a fruit to get information.")
+else:
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e:
+ streamlit.error()
+
+#streamlit.write('The user entered ', fruit_choice)
+
+
+
 #streamlit.text(fruityvice_response.json()) #just writes data to screen
 
 # takes the json version of response and normalizes it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it the screen as a table
-streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 
